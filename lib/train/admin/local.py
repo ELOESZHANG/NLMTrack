081d27cc4d759a2b44ee54853df9d8d6209c7151
ELOESZHANG <46095890+ELOESZHANG@users.noreply.github.com>
2024-03-22 16:25:40 +0800

    Update local.py

--- lib/train/admin/local.py
+++ lib/train/admin/local.py
@@ -1,10 +1,10 @@
 class EnvironmentSettings:
     def __init__(self):
-        self.workspace_dir = '/media/SSDPA/yanmiao/rgb/NLMTTrack'    # Base directory for saving network checkpoints.
-        self.tensorboard_dir = '/media/SSDPA/yanmiao/rgb/NLMTTrack/got_tensorboard/tensorboard_NLMTRACK'  # Directory for tensorboard files.
-        self.lsotb_tir_dir = '/media/SSDPA/yanmiao/LSOTB-TIR_TrainingData'  # LSOTB-TIR training dataset path
-        self.lsot_tir_dir = '/media/SSDPA/yanmiao/sequences'  # LSOTB-TIR evaluation dataset path
-        self.got10k_dir = '/media/HDDP/VOT/datasets/GOT-10k/train'  # GOT-10K training dataset path
+        self.workspace_dir = ''    # Base directory for saving network checkpoints.
+        self.tensorboard_dir = ''  # Directory for saving tensorboard files.
+        self.lsotb_tir_dir = ''  # LSOTB-TIR training dataset path
+        self.lsot_tir_dir = ''  # LSOTB-TIR evaluation dataset path
+        self.got10k_dir = ''  # GOT-10K training dataset path
         self.lasher_dir = ''
 
 
