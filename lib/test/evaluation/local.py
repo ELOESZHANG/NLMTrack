0d133def0f1f776eb203733b18b78ebb0d60f8ba
ELOESZHANG <46095890+ELOESZHANG@users.noreply.github.com>
2024-03-22 16:28:00 +0800

    Update local.py

--- lib/test/evaluation/local.py
+++ lib/test/evaluation/local.py
@@ -6,13 +6,13 @@ def local_env_settings():
     # Set your local paths here.
 
     settings.davis_dir = ''
-    settings.network_path = '/home/ym/work/track/RGB/VideoX-master/NLMTrack/test/networks'    # Where tracking networks are stored.
-    settings.prj_dir = '/media/SSDPA/yanmiao/rgb/NLMTrack'
-    settings.result_plot_path = '/media/SSDPA/yanmiao/rgb/NLMTrack/test/result_plots'
-    settings.results_path = '/media/SSDPA/yanmiao/rgb/NLMTrack/results/ptb_tir_047'  # Where to store tracking results
-    settings.save_dir = '/media/SSDPA/yanmiao/rgb/NLMTrack'
-    settings.lsotb_tir_path = '/media/SSDPA/yanmiao/sequences'
-    settings.ptb_tir_path = '/media/SSDPA/yanmiao/ptbtir'
+    settings.network_path = ''    # Where tracking networks are stored.
+    settings.prj_dir = ''    # project path
+    settings.result_plot_path = ''
+    settings.results_path = ''  # Where to store tracking results
+    settings.save_dir = ''
+    settings.lsotb_tir_path = ''
+    settings.ptb_tir_path = ''
     settings.vot1517_path = ''
 
     return settings
